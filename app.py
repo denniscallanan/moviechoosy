c45f5a5a9c14f8c8573e778d9bab5537841cdb1c
Greg Peckory <gregspeckory@gmail.com>
2016-12-15 23:26:12 +0000

    nextcom

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, psycopg2
 
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -6,7 +6,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+
+	data = []
+
+	conn = psycopg2.connect(
+
+	database="da301k7aqru948", 
+	user="oaskaojnytkyke", 
+	password="e71735b255a8b198f17966271880fa5658a98e18a507d0ae776e38ed6c09ec3d", host="ec2-54-247-119-245.eu-west-1.compute.amazonaws.com",
+	port="5432"
+
+	)
+
+	cur = conn.cursor()
+
+	cur.execute("select * from movies limit 10")
+
+	data = cur.fetchall()
+
+	conn.close()
+
+
+
+	return render_template('index.html', vals = data)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
